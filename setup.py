53375ac730b517e5dd2ef849171384677e7bb9b8
Saqib <msaqib52@gmail.com>
2016-05-26 14:49:52 +0500

    course_metadata app extracted from edx-platform

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='course-edx-platform-extensions',
+    version='1.0.0',
+    description='Course metadata management extension for edX platform',
+    long_description=open('README.rst').read(),
+    author='edX',
+    url='https://github.com/edx-solutions/course-edx-platform-extensions.git',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "django>=1.4",
+    ],
+)
