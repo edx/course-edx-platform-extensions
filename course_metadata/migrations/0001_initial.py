a685b5cd3b73001c58d9ef8edf413e02e60c1fcb
Ayesha Baig <ayesha.baig@arbisoft.com>
2017-03-09 12:10:58 +0500

    Fixes for ficus upgrade

--- course_metadata/migrations/0001_initial.py
+++ course_metadata/migrations/0001_initial.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 import django.utils.timezone
 import model_utils.fields
-import xmodule_django.models
+from openedx.core.djangoapps.xmodule_django.models import CourseKeyField
 
 
 class Migration(migrations.Migration):
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
                 ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
-                ('id', xmodule_django.models.CourseKeyField(max_length=255, serialize=False, primary_key=True, db_index=True)),
+                ('id', CourseKeyField(max_length=255, serialize=False, primary_key=True, db_index=True)),
                 ('total_modules', models.IntegerField(default=0)),
                 ('total_assessments', models.IntegerField(default=0)),
             ],
