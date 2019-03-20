2f63ccf9abf5715f496e041588796d0336493f6a
Kshitij Sobti <kshitij@sobti.in>
2019-03-20 14:46:31 +0530

    Bump up version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='course-edx-platform-extensions',
-    version='1.1.1',
+    version='2.0.0',
     description='Course metadata management extension for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
