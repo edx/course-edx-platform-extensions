99a4d629892fb6a7e750bd5b84e87178ec03bde5
Shafqat Farhan <shafqat.farhan@arbisoft.com>
2019-10-28 15:28:14 +0500

    Django version upgraded to 1.11

--- course_metadata/urls.py
+++ course_metadata/urls.py
@@ -1,15 +1,14 @@
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from . import views
 
 COURSE_ID_PATTERN = settings.COURSE_ID_PATTERN
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(
         r'^{}/settings$'.format(COURSE_ID_PATTERN),
         views.CourseSettingView.as_view(),
         name='additional-course-settings',
     ),
-)
+]
